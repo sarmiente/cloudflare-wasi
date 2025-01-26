3df3b0949afba22cb0d241fa75247b841e49f9e6
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-01-26 10:58:35 -0500

    commits about many things

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,31 +3,81 @@ export const languages = {
     es: 'Español',
   };
   
-  export const defaultLang = 'es';
+  export const defaultLang = 'en';
 
   export const showDefaultLang = false;
   
   export const ui = {
     en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-      'nav.twitter': 'Twitter',
+      'layout.title': 'Short Term Rental Apartments, Houses, Yachts & Boats',
+      'layout.description' : 'Discover Exclusive Short Term Rental Apartments, Houses and Yachts. Turn every trip into something truly unforgettable.',
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.buy': 'Buy',
+        'nav.rent': 'Rent',
+        'nav.services': 'Services',
+        'nav.manage': 'Property Management',
+        'nav.manage-desc': 'Optimize the value of your properties with our comprehensive and personalized management.',
+        'nav.concierge' : 'Luxury Concierge',
+        'nav.concierge-desc': 'Enjoy exclusive and personalized experiences with our luxury concierge service.',
+        'nav.short': 'Short Term Rentals',
+        'nav.short-desc': 'Discover our exclusive collection of short-term rental apartments and houses, specially designed for you.',
+        'nav.daily': 'Daily Assistance',
+        'nav.tourist': 'Tourist Plans',
+        'nav.experiences': 'Experiences',
+        'nav.transport': 'Transportation',
+        'nav.land': 'Land',
+        'nav.nautical': 'Nautical',
+        'nav.who': 'Who We Are',
+        'nav.team': 'Our Team',
+        'nav.contact': 'Contact',
+        'nav.links': 'Useful Links',
+        'nav.msg': 'Your home away home',
+        'nav.terms': 'Terms & Conditions',
+        'nav.accept': 'I accept',
+        'nav.for': 'for processing personal data',
+        'nav.privacy': 'Privacy Policy',
+        'nav.pqrs': 'Q&A',
+        'nav.support': 'Support',
+        'nav.rights': 'All rights reserved',
+        'nav.more': 'Learn More'
+      
     },
     es: {
-      'nav.home': 'Inicio',
-      'nav.about': 'Nosotros',
-      // Agregar traducción faltante para Twitter
-      'nav.twitter': 'Twitter',
+      'layout.title' : 'Apartamentos, Casas, Yates y Barcos de Alquiler a Corto Plazo',
+      'layout.description': 'Descubre exclusivos Apartamentos, Casas, Yates y Barcos. Vive experiencias únicas y convierte cada viaje en algo realmente inolvidable.',
+        'nav.home': 'Inicio',
+        'nav.about': 'Nosotros',
+        'nav.buy': 'Comprar',
+        'nav.rent': 'Alquilar',
+        'nav.services': 'Servicios',
+        'nav.manage': 'Gestión de Propiedades',
+        'nav.manage-desc': 'Optimiza el valor de tus inmuebles con nuestra gestión integral y personalizada.',
+        'nav.concierge' : 'Conserjería de Lujo',
+        'nav.concierge-desc': 'Disfruta de experiencias exclusivas y personalizadas con nuestro servicio de conserjería de lujo.',
+        'nav.short': 'Alquileres a corto plazo',
+        'nav.short-desc': 'Descubre nuestra exclusiva colección de apartamentos y casas de alquiler a corto plazo, diseñadas especialmente para ti.',
+        'nav.daily': 'Asistencia diaria',
+        'nav.tourist': 'Planes turisticos',
+        'nav.experiences': 'Experiencias',
+        'nav.transport': 'Transporte',
+        'nav.land': 'Terrestre',
+        'nav.nautical': 'Nautico',
+        'nav.who': 'Quiénes Somos',
+        'nav.team': 'Nuestro Equipo',
+        'nav.contact': 'Contacto',
+        'nav.links': 'Enlaces útiles',
+        'nav.msg': 'Tu hogar lejos de casa',
+        'nav.terms': 'Términos y Condiciones',
+        'nav.accept': 'Acepto',
+        'nav.for': 'para el procesamiento de datos personales',
+        'nav.privacy': 'Política de Privacidad',
+        'nav.pqrs': 'PQRS',
+        'nav.support': 'Soporte',
+        'nav.rights': 'Todos los derechos reservados',
+        'nav.more': 'Saber más'
+    
     },
   } as const;
 
-  export const routes = {
-    es: {
-      'about': 'nosotros',
-      'services': 'servicios'
-    },
-    en: {
-      'about': 'about',
-      'services': 'services'
-    },
-  }
+  
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,54 +1,20 @@
+import { ui, defaultLang, showDefaultLang } from './ui';
 
-import { ui, defaultLang, showDefaultLang, routes  } from './ui';
-export type RouteKey = keyof typeof routes.es; // 'about' | 'services'
 
 export function getLangFromUrl(url: URL) {
-    const [, lang] = url.pathname.split('/');
-    if (lang in ui) return lang as keyof typeof ui;
-    return defaultLang;
-  }
-  
-  export function useTranslations(lang: keyof typeof ui) {
-    return function t(key: keyof typeof ui[typeof defaultLang]) {
-      return ui[lang][key] || ui[defaultLang][key];
-    }
-  }
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
 
-
-  export function useTranslatedPath(lang: keyof typeof ui) {
-    return function translatePath(path: `/${RouteKey}/` | string, l: string = lang) {
-        const pathName = path.replaceAll('/', '') as RouteKey;
-        
-      
-      const hasTranslation = defaultLang !== l && 
-        routes[l as keyof typeof routes] !== undefined && 
-        routes[l as keyof typeof routes][pathName] !== undefined;
-      
-      const translatedPath = hasTranslation 
-        ? '/' + routes[l as keyof typeof routes][pathName] 
-        : path;
-  
-      return !showDefaultLang && l === defaultLang 
-        ? translatedPath 
-        : `/${l}${translatedPath}`;
-    }
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
   }
+}
 
-  export function getRouteFromUrl(url: URL): string | undefined {
-    const pathname = new URL(url).pathname;
-    const parts = pathname?.split('/');
-    const path = parts.pop() || parts.pop() as RouteKey; // Type assertion
-  
-    if (path === undefined) return undefined;
-  
-    const currentLang = getLangFromUrl(url);
-  
-    if (defaultLang === currentLang) {
-      return routes[defaultLang][path as RouteKey] || undefined;
+export function useTranslatedPath(lang: keyof typeof ui) {
+    return function translatePath(path: string, l: string = lang) {
+      return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
     }
-  
-    const reversedKey = Object.entries(routes[currentLang])
-      .find(([_, value]) => value === path)?.[0];
-  
-    return reversedKey as RouteKey | undefined;
   }
